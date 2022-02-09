25ba4f21b8f41a29240466ddbb7d1975c74b8295
elearningbasic <74741371+elearningbasic@users.noreply.github.com>
2022-02-10 02:14:08 +0300

    1

---
+++ story_content/user.js
@@ -0,0 +1,444 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5yiQVSN26aE":
+        Script1();
+        break;
+      case "5WJQAqfcP0g":
+        Script2();
+        break;
+      case "6gDl8E3tVk7":
+        Script3();
+        break;
+      case "66JgBdPkd0c":
+        Script4();
+        break;
+      case "6dIDKmZyVkX":
+        Script5();
+        break;
+      case "5rxbfRtkPnC":
+        Script6();
+        break;
+      case "6GpEirUWd5I":
+        Script7();
+        break;
+      case "5noqXn24ihM":
+        Script8();
+        break;
+      case "5Y17W8is1l6":
+        Script9();
+        break;
+      case "6MOgRUX166w":
+        Script10();
+        break;
+      case "6XbkqnZPBRv":
+        Script11();
+        break;
+      case "6ggcFrZmTMf":
+        Script12();
+        break;
+      case "5y2Q1WcGxvL":
+        Script13();
+        break;
+      case "5iAodO6F6oU":
+        Script14();
+        break;
+      case "6Hi4uNdLdEr":
+        Script15();
+        break;
+      case "5p20w0c9tG1":
+        Script16();
+        break;
+      case "6a89LIpUp2L":
+        Script17();
+        break;
+      case "5WvYGPERiAd":
+        Script18();
+        break;
+      case "6XaTJ871DQs":
+        Script19();
+        break;
+      case "61vIOYiztkW":
+        Script20();
+        break;
+      case "6c73nPVFxYj":
+        Script21();
+        break;
+      case "5cMMTLKoF7k":
+        Script22();
+        break;
+      case "5a4c65fxTak":
+        Script23();
+        break;
+      case "6QEGLmt8t5T":
+        Script24();
+        break;
+      case "64SGHkLA1md":
+        Script25();
+        break;
+      case "6Rk9QXKXbAC":
+        Script26();
+        break;
+      case "6aeTpBAf8ed":
+        Script27();
+        break;
+      case "6j9H3r1mA4V":
+        Script28();
+        break;
+      case "5wsWVK3yRnU":
+        Script29();
+        break;
+      case "6Qy4LZ8t8Ew":
+        Script30();
+        break;
+      case "5mJH6Wrva1P":
+        Script31();
+        break;
+      case "6NwVRrGPdhQ":
+        Script32();
+        break;
+      case "5s7d6vTzzxU":
+        Script33();
+        break;
+      case "6PouZbKxJPc":
+        Script34();
+        break;
+      case "6aa7OOJOULl":
+        Script35();
+        break;
+      case "6NZgXS5Robg":
+        Script36();
+        break;
+      case "6egfIyDYh3l":
+        Script37();
+        break;
+      case "5gB50BsyrgD":
+        Script38();
+        break;
+      case "6OtY2p2OPYO":
+        Script39();
+        break;
+      case "5sNAhp0wn27":
+        Script40();
+        break;
+  }
+}
+
+function Script1()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script2()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script3()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script4()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script5()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script6()
+{
+  document.getElementsByTagName("video")[0].setAttribute("controls", "true");
+}
+
+function Script7()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script8()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script9()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script10()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script11()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script12()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script13()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script14()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script15()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script16()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script17()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script18()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script19()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script20()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script21()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script22()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script23()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script24()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script25()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script26()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script27()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script28()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script29()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script30()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script31()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script32()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script33()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script34()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script35()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script36()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script37()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script38()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script39()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
+function Script40()
+{
+  let allVideosInCourse = document.getElementsByTagName("video"); 
+for (let i = 0; i < allVideosInCourse.length; i++) {
+  allVideosInCourse[i].setAttribute("controls", "true");
+}
+}
+
